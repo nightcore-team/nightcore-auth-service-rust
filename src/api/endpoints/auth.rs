ede3dfd5181822a06200ce1342d49c68af53d2fc
monok8i <didkivskiyyura725@gmail.com>
2026-06-24 00:45:40 +0200

    fix: return Redirect from login_handler and use Arc<GlobalState> State extractor

--- src/api/endpoints/auth.rs
+++ src/api/endpoints/auth.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use axum::{extract::State, response::Redirect};
 
 use crate::api::state::GlobalState;
@@ -6,8 +8,8 @@ pub async fn refresh_token_handler() {}
 
 pub async fn logout_handler() {}
 
-pub async fn login_handler(State(state): State<GlobalState>) {
-    Redirect::temporary(&state.oic_service.oauth_provider.get_authorization_url());
+pub async fn login_handler(State(state): State<Arc<GlobalState>>) -> Redirect {
+    Redirect::temporary(&state.oic_service.oauth_provider.get_authorization_url())
 }
 
 pub async fn discord_callback_handler() {}
